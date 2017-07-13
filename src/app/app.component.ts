e1c127f840223f5798dbde0f7d79aa4088095971
boicualexandru <boicualexandru@mail.com>
2017-07-13 20:38:52 +0300

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { InvoicesService } from "app/services/invoices.service";
+import { Invoice } from "app/models/invoice";
+import { MenuItem } from "primeng/primeng";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  invoices: Invoice[] = [];
+  errorMessage: string = '';
+
+  displayDialog: boolean;
+  newInvoice: Invoice;
+
+  selectedInvoice: Invoice;
+  items: MenuItem[];
+
+  constructor(private invoicesService: InvoicesService) { }
+
+  ngOnInit() {
+    this.invoicesService.getAll()
+      .subscribe(
+      invoices => this.invoices = invoices,
+      error => this.errorMessage = <any>error
+      );
+
+    this.items = [
+      { label: 'Delete', icon: 'fa-close', command: (event) => this.deleteInvoice(this.selectedInvoice) }
+    ];
+  }
+
+  showDialogToAddItem() {
+    this.newInvoice = new Invoice();
+    this.displayDialog = true;
+  }
+
+  save() {
+    let invoices = [...this.invoices];
+    invoices.push(this.newInvoice);
+
+    this.invoices = invoices;
+    this.newInvoice = null;
+    this.displayDialog = false;
+  }
+
+  deleteInvoice(invoice: Invoice){
+    let index = -1;
+    for (let i = 0; i < this.invoices.length; i++) {
+      if (this.invoices[i].Id == invoice.Id) {
+        index = i;
+        break;
+      }
+    }
+    debugger;
+    this.invoices = this.invoices.filter((val,i) => i!=index);
+  }
 }
---
+++ src/app/services/base.service.ts
@@ -0,0 +1,28 @@
+import { Injectable }              from '@angular/core';
+import { Http, Response }          from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class BaseService{
+    constructor(protected http: Http) {}
+    
+    protected extractData(res: Response) {
+        let body = res['_body'];
+        return JSON.parse(body) || { };
+    }
+    protected handleError (error: Response | any) {
+        let errMsg: string;
+        if (error instanceof Response) {
+        const body = error.json() || '';
+        const err = body.error || JSON.stringify(body);
+        errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+        } else {
+        errMsg = error.message ? error.message : error.toString();
+        }
+        console.error(errMsg);
+        return Observable.throw(errMsg);
+    }
+}
